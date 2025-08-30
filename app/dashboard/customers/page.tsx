e1ca90592b7c7b4352288d973d3f1d6e0662abf3
streetsdark <mdavidcha@gmail.com>
2025-08-31 00:45:06 +0200

    Primer commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+/*
+Existen varias formas de dar estilos en Next.js, una de ellas es usando CSS Modules.
+*/
+/*
+orden de importaciones:
+1. Librerias externas
+2. Componentes internos
+3. Estilos (css, sass, scss)
+4. Imagenes y otros archivos
+*/
+return <p>Customers Page</p>;
+}
